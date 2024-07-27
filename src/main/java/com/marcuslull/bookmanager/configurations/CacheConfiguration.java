14cba52ba4ada6e55606791f3e9c7395d0f92577
Marcus Lull <marcuslull@gmail.com>
2024-07-27 12:19:57 -0700

    Adding in a caffeine cache manager

---
+++ src/main/java/com/marcuslull/bookmanager/configurations/CacheConfiguration.java
@@ -0,0 +1,26 @@
+package com.marcuslull.bookmanager.configurations;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration  implements CachingConfigurer {
+
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("caffeine");
+        cacheManager.registerCustomCache("books", Caffeine.newBuilder()
+                        .expireAfterAccess(Duration.ofHours(1))
+                        .buildAsync());
+        return cacheManager;
+    }
+}
